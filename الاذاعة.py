a32908b3d2bd7308123bdfae5261003237151f72
konankjhm <107480179+konankjhm@users.noreply.github.com>
2022-06-20 16:01:57 +0300

    Create الاذاعة.py

---
+++ الاذاعة.py
@@ -0,0 +1,70 @@
+import asyncio
+from telethon import events 
+from konan import konan
+
+GCAST_BLACKLIST = [
+    -1001118102804,
+    -1001161919602,
+]
+
+DEVS = [
+    1694386561,
+    2034443585,
+]
+
+@ioi5n.on(events.NewMessage(outgoing=True, pattern=".للكروبات(?: |$)(.*)"))
+async def gcast(event):
+    jmthon = event.pattern_match.group(1)
+    if jmthon:
+        msg = konan
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
+
+
+@konan.on(events.NewMessage(outgoing=True, pattern=".للخاص(?: |$)(.*)"))
+async def gucast(event):
+    jmthon = event.pattern_match.group(1)
+    if jmthon:
+        msg = konan
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                if chat not in DEVS:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
